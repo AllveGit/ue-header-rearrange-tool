35ccdd0ba74b02e439b0103dbb5cdb41e5a0e0dd
Allve <allvegamedevelop@gmail.com>
2023-06-14 01:13:16 +0900

    include문 formatting 테스트 작업

--- src/convert-header.py
+++ src/convert-header.py
@@ -27,11 +27,40 @@ for file_name in os.listdir(config_source_path):
     if os.path.isdir(absoulte_path):
         config_module_names.append(file_name)
 
+# Header include rearrange functions
+def search_header(header_path, search_path):
+    result = False
+    filenames = os.listdir(search_path)
+    for filename in filenames:
+        full_filepath = search_path + '/' + filename
+        if header_path in full_filepath:
+            result = True
+            break
+        elif os.path.isdir(full_filepath) and result == False:
+            if search_header(header_path, full_filepath):
+                result = True
+                break
+
+    return result
+
+def get_module_from_header(header_path, source_path, module_names):
+    if 'generated.h' in header_path:
+        return 'Gen'
+    else:
+        for module_name in module_names:
+            full_module_path = source_path + '/' + module_name
+            if (search_header(header_path, full_module_path)):
+                return module_name
+
+    return 'UE'
+
 # Parsing source code to header rearrange
 is_collect_start = False
 is_commenting = False
 
-f = open('E:/Programming/Projects/Unreal/AreaOfOperations/Project/Source/AOGame/Character/Component/AOCharacterMovementComponent.h', 'r', encoding='utf-8')
+include_headers = dict()
+
+f = open('E:/Programming/Projects/Unreal/AreaOfOperations/Project/Source/AOGame/Character/Actor/AOCharacter.h', 'r', encoding='utf-8')
 for line in f.readlines():
     if is_collect_start:
         nospace_line = line.replace(" ", "")
@@ -49,7 +78,13 @@ for line in f.readlines():
         if '#include' in line:
             header_path = line.strip('#include ')
             header_path = header_path.replace('\n', '')
-            print(header_path)
+            header_path = header_path.replace('"', '')
+            
+            header_module_name = get_module_from_header(header_path, config_source_path, config_module_names)
+            if header_module_name in include_headers:
+                include_headers[header_module_name].append(line)
+            else:
+                include_headers[header_module_name] = [line]
         else:
             is_collect_start = False
             print('Collect End')
@@ -57,6 +92,12 @@ for line in f.readlines():
         is_collect_start = True
         print('Collect Start')
         
+# Formating
+for include_module_name in include_headers:
+    print('//' + include_module_name)
+    for include_header_line in include_headers[include_module_name]:
+        print(include_header_line, end='') 
+    print('\n', end='')
 
 # Test
 print(config_source_path)
